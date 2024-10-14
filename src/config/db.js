77a9cc406a804baa5777ac86a7916c87f74a99e9
Archana1913 <archanaop27@gmail.com>
2024-10-14 21:31:33 +0530

    Delete src directory

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const  mongoose  = require("mongoose")
-
-const mongodbUrl= "mongodb+srv://archanaop27:zlOxVsMRQcA7z6NI@cluster0.b9awp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-async function connectDB(){
-    return mongoose.connect(mongodbUrl)
-}
-
-module.exports = connectDB
--- src/routes/homeRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const router=Router();
-
-router.get("",async (req,res) => {
-    res.status(200).send({message:"welcome to online food ordering website!"});
-})
-
-module.exports=router;
